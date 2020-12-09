da4d5ad94d18f3f3caafe2a73000bae9a1e16624
DESKTOP-6MRRTJ2\邹  皓 <1760756718@qq.com>
2020-12-09 16:06:24 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 加载全局样式文件
+import './styles/index.less'
 Vue.config.productionTip = false
+
+// 创建根实例
+// 把router配置到根实例中
 
 new Vue({
   router,
